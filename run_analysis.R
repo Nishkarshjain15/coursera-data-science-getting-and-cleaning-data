d2d27b488db1580b53a5a0b5ea6ff11ef6e6394b
Gonzalo Peña <goanpeca@gmail.com>
2014-07-28 02:37:08 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,23 @@
 ## (c) 2014 Gonzalo PENA C.
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names. 
 
+# This script follows a step by step procedure in order to transform the dataset from
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+# into a tidy subset dataset as part of the course project in the Coursera:
+# Data Science Specialization: Getting and Cleaning Data
+# The script is divided in blocks that explain each one of the steps. 
+# During this process variables were sensible names to easily identify the parts.
 
-# @ Set the current work directory ####
+# @ Script preprocessing
+# Set the current work directory ####
 script.dir <- dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 
-# @ Define base folders and subfolder names ####
+# Define base folders and subfolder names ####
 baseFolder <- 'UCI HAR Dataset'
 trainFolder <- 'train'
 testFolder <- 'test'
 
-# @ Download data set and unzip it ####
+# Download data set and unzip it ####
 # But first check if the folder exists to skipt this step
 if (!file.exists(baseFolder)) {
     dataUrl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
@@ -21,7 +26,7 @@ if (!file.exists(baseFolder)) {
     unzip(tempFile)
 }
 
-# @ Define filenames to load ####
+# Define filenames to load ####
 activityFilename <- 'activity_labels.txt'
 featuresFilename <- 'features.txt'
 trainSubjectFilename <- 'subject_train.txt'
@@ -31,7 +36,7 @@ testSubjectFilename <- 'subject_test.txt'
 testLabelsFilename <- 'y_test.txt'
 testDataFilename <- 'X_test.txt'
 
-# **Create path to files ####
+# Create path to files ####
 activityFilename <- file.path(baseFolder, activityFilename)
 featuresFilename <- file.path(baseFolder, featuresFilename)
 trainLabelsFilename <- file.path(baseFolder, trainFolder, trainLabelsFilename)
@@ -41,6 +46,7 @@ testLabelsFilename <- file.path(baseFolder, testFolder, testLabelsFilename)
 testSubjectFilename <- file.path(baseFolder, testFolder, testSubjectFilename)
 testDataFilename <- file.path(baseFolder, testFolder, testDataFilename)
 
+# @ Script processing ####
 # Read files into R data frames ####
 activity <- read.table(activityFilename, col.names=c('Number', 'Activity'))
 features <- read.table(featuresFilename, col.names=c('Number', 'Feature'))
@@ -61,25 +67,25 @@ features$Feature <- gsub('^angle', 'Angle.', features$Feature)
 features$Feature <- gsub('mean', 'Mean', features$Feature)
 features$Feature <- gsub('tBody', 'TimeBody', features$Feature)
 
-# @ Change the name of the data sets using the features data ####
+# Change the name of the data sets using the features data ####
 colnames(testData) <- features$Feature
 colnames(trainData) <- features$Feature
 
-# @ Replace train and test labels by the names in the activity file ####
+# Replace train and test labels by the names in the activity file ####
 labels <- activity$Activity
 testFactors <- factor(testLabels$Number)
 trainFactors <- factor(trainLabels$Number)
 testActivity <- data.frame(Activity=as.character(factor(testFactors, labels=labels)))
 trainActivity <- data.frame(Activity=as.character(factor(trainFactors, labels=labels)))
 
-# @ Merge data using column binds ####
+# Merge data using column binds ####
 testMergedData <- cbind(testSubject, testActivity, testData)
 trainMergedData <- cbind(trainSubject, trainActivity, trainData)
 
 # @ Merges the training and the test sets to create one data set ####
 mergedData <- rbind(testMergedData, trainMergedData)
 
-# Remove measurements that contain Angle or MeanFreq and store the rest of the columns in rows
+# Remove measurements that contain Angle or MeanFreq and store the rest of the columns in rows ####
 rows <- c()
 colNames <- colnames(mergedData)
 for (i in seq_along(colNames)){
@@ -91,12 +97,12 @@ for (i in seq_along(colNames)){
     }
 } 
 
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Extracts only the measurements on the mean and standard deviation for each measurement #### 
 mergedData <- mergedData[,rows]
 mergedDataSubset <- mergedData[,grep('Subject|Activity|Mean|std',x=colnames(mergedData))]
 
 # @ Creates a second, independent tidy data ####
-# set with the average of each variable for each activity and each subject. 
+# set with the average of each variable for each activity and each subject ####
 library(data.table)
 tidyData <- data.table(mergedDataSubset)
 tidyData <- tidyData[,lapply(.SD, mean), by=c('Subject', 'Activity')]
@@ -109,4 +115,5 @@ write.table(tidyData, file=tidyFileName, row.names=FALSE)
 # @ For test purposes read the created file ####
 tidyDataRead <- read.csv(tidyFileName, sep=' ')
 
+# @ For CodeBook.md creation write column names of data set ####
 write.table(colnames(mergedDataSubset), 'm.md', row.names=FALSE)
